50d970278a01a788f99f1c4ab2dba984e1a64b72
Stinger-22 <maxym.sobol@gmail.com>
2025-01-29 19:00:16 +0100

    Entity don't use LocalDate, but plain String. It's more convenient for different date formats. BookingRequest doesn't use primitive types so that it's possible to pass null values.

--- src/test/java/com/restfulbooker/api/requests/entities/BookingDates.java
+++ src/test/java/com/restfulbooker/api/requests/entities/BookingDates.java
@@ -1,12 +1,6 @@
 package com.restfulbooker.api.requests.entities;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-
-import java.time.LocalDate;
-
-public record BookingDates(@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-                           LocalDate checkin,
-                           @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-                           LocalDate checkout) {
+public record BookingDates(String checkin,
+                           String checkout) {
 
 }
--- src/test/java/com/restfulbooker/api/requests/entities/BookingRequest.java
+++ src/test/java/com/restfulbooker/api/requests/entities/BookingRequest.java
@@ -2,9 +2,16 @@ package com.restfulbooker.api.requests.entities;
 
 public record BookingRequest(String firstname,
                              String lastname,
-                             int totalprice,
-                             boolean depositpaid,
+                             Integer totalprice,
+                             Boolean depositpaid,
                              BookingDates bookingdates,
                              String additionalneeds) {
 
+    public static BookingRequest defaultEntity() {
+        return new BookingRequest("Sam", "Gamgee", 140, true,
+                new BookingDates(
+                        "2025-02-14",
+                       "2025-03-01"
+                ), "Breakfast. Second breakfast");
+    }
 }
