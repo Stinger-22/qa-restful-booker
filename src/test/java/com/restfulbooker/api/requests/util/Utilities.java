640113d14895c9d04ccd3770104f8332607d8add
Stinger-22 <maxym.sobol@gmail.com>
2025-01-29 19:51:16 +0100

    Deleted AssumedUtils. Token setting up is now located in Utilities.

--- src/test/java/com/restfulbooker/api/requests/util/Utilities.java
+++ src/test/java/com/restfulbooker/api/requests/util/Utilities.java
@@ -1,10 +1,42 @@
 package com.restfulbooker.api.requests.util;
 
+import com.restfulbooker.api.requests.AuthAPI;
+import com.restfulbooker.api.requests.BookingAPI;
 import com.restfulbooker.api.requests.entities.Token;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 public class Utilities {
+    public static String receiveCookieToken() {
+        RequestSpecification auth = AuthAPI.createToken("admin", "password123");
+        Response response = given(auth).post(AuthAPI.PATH);
+        assumeTrue(response.statusCode() == 200); // TODO throw own exception
+        return extractToken(response);
+    }
+
     public static String extractToken(Response response) {
         return response.body().as(Token.class).token();
+    }
+
+    public static void setCookieToken(RequestSpecification request, String token) {
+        request.cookie("token", token);
+    }
+
+    public static void deleteAllBookings() {
+        RequestSpecification getIds = given(BookingAPI.getBookingIds());
+        Response response = given(getIds).get("/booking");
+
+        String token = receiveCookieToken();
+        List<Integer> createdBookingsIds = response.then().extract().path("bookingid");
+        for (int id : createdBookingsIds) {
+            RequestSpecification deleteBooking = given(BookingAPI.deleteBooking(id));
+            setCookieToken(deleteBooking, token);
+            given(deleteBooking).delete("/booking/{id}");
+        }
     }
 }
