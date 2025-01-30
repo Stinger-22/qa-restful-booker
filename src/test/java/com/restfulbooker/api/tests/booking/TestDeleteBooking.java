153b033d9c510ba0730f6e39496fdf7ac11992b5
Stinger-22 <maxym.sobol@gmail.com>
2025-01-30 19:40:45 +0100

    Wrote tests for booking deleting.

--- src/test/java/com/restfulbooker/api/tests/booking/TestDeleteBooking.java
+++ src/test/java/com/restfulbooker/api/tests/booking/TestDeleteBooking.java
@@ -1,27 +1,73 @@
 package com.restfulbooker.api.tests.booking;
 
 import com.restfulbooker.api.requests.BookingAPI;
+import com.restfulbooker.api.requests.util.Utilities;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.given;
+
 public class TestDeleteBooking extends BookingAPI {
+    private static final String PATH = "/booking/{id}";
+
+    private static ResponseSpecification successfullyDeleted;
+
+    @BeforeAll
+    static void buildExpectedResponses() {
+        successfullyDeleted = new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .build();
+    }
+
     @Test
     void deleteBookingWithCookieToken() {
-        throw new RuntimeException("Not implemented");
+        int id = Utilities.createDefaultBooking().bookingid();
+
+        String token = Utilities.receiveCookieToken();
+        RequestSpecification deleteBooking = given(BookingAPI.deleteBooking(id));
+        Utilities.setCookieToken(deleteBooking, token);
+        given(deleteBooking)
+        .when()
+            .delete(PATH)
+        .then()
+            .spec(successfullyDeleted);
     }
 
     @Test
     void deleteBookingWithAuthToken() {
-        throw new RuntimeException("Not implemented");
+        int id = Utilities.createDefaultBooking().bookingid();
+
+        RequestSpecification deleteBooking = given(BookingAPI.deleteBooking(id));
+        Utilities.setAuthToken(deleteBooking);
+        given(deleteBooking)
+        .when()
+            .delete(PATH)
+        .then()
+            .spec(successfullyDeleted);
     }
 
     @Test
     void deleteBookingWithoutToken() {
-        throw new RuntimeException("Not implemented");
+        int id = Utilities.createDefaultBooking().bookingid();
+
+        given(deleteBooking(id))
+        .when()
+            .delete(PATH)
+        .then()
+             .statusCode(403);
+
+        Utilities.deleteBooking(id);
     }
 
     @Test
     void deleteNonExistingBooking() {
-        throw new RuntimeException("Not implemented");
+        given(deleteBooking(1))
+        .when()
+            .delete(PATH)
+        .then()
+            .statusCode(404);
     }
-
 }
